3f23c151c34c0be5ce6ee76f6e8072c59f677c48
Jake Vasilakes <jvasilakes@gmail.com>
2015-03-03 12:53:48 +0000

    Output RDF now even more valid

--- converter.py
+++ converter.py
@@ -18,7 +18,7 @@ import post
 
 data_file = "Posts.xml"
 
-rdf_header = u'''@prefix dc: <http://purl.org.dc/elements/1.1/> .
+rdf_header = u'''@prefix dc: <http://purl.org/dc/elements/1.1/> .
 @prefix sioc: <http://rdfs.org/sioc/ns#> .
 @prefix tsioc: <http://rdfs.org/sioc/types#> .
 '''
@@ -32,7 +32,7 @@ def main():
     # Separate posts into questions and answers.
     # Classify based on existence of 'AnswerCount' (questions)
     #   or 'ParentId' (answers). If neither, skip it (there are
-    #   some random posts that aren't either.
+    #   some random posts that aren't either).
     questions = []
     answers = []
     for child in root:
@@ -51,33 +51,35 @@ def main():
         for q in questions:
             # Check to which question this is an answer.
             if a.parent_Id == q.Id:
-                q.attribs[u"tsioc:Answer"].append(a)
+                q.attribs[u"sioc:has_reply"].append(a)
                 a.attribs[u"sioc:has_parent"] = q
                 # Check if this is the best answer to that question.
                 try:
                     # Quotes around a.Id are needed for a valid comparison.
-                    if '"' + a.Id + '"' == q.attribs[u"tsioc:BestAnswer"]:
-                        q.attribs[u"tsioc:BestAnswer"] = a
+                    if a.Id == q.best_answer_Id:
+                        #a.type_str = u"tsioc:BestAnswer"
+                        a.best_answer = True
                 except:
                     pass
 
     # Write some turtle!
     # See notes.txt for what I have to change about this.
-    with open("outfile.ttl", "w") as outfile:
+    with open("arduino_rdf.ttl", "w") as outfile:
 
         # First write the header.
         outfile.write(rdf_header + '\n')
 
-        for q in questions[:5]:
+        for q in questions:
             for attrib in q.get_attribs_string():
                 outfile.write(attrib)
-            outfile.write('\n')
+            outfile.write('\n\n')
 
-        for a in answers[:5]:
+        for a in answers:
             for attrib in a.get_attribs_string():
                 outfile.write(attrib)
-            outfile.write('\n')
+            outfile.write('\n\n')
 
+        
 
 if __name__ == '__main__':
     main()
--- post.py
+++ post.py
@@ -9,18 +9,18 @@ def create_uri(id_string):
     '''
     Creates a new uri from an Id number.
     '''
-    return u'<http://vocab.inf.ed.ac.uk/sws#{}>' .format(id_string)
+    return u'<http://vocab.inf.ed.ac.uk/sws#post{}>' .format(id_string)
 
 def format_str(string):
     '''
     Takes care of formatting strings to be valid RDF.
     '''
+    # Esacpe backslashes.
+    string = string.replace('\\', '\\\\')
     # Check for double quotes and escape them.
     string = string.replace('"', '\\"')
-
     # Remove newlines.
     string = string.replace('\n', '')
-
     # Prepend/append double quotes.
     string = '"' + string + '"'
 
@@ -36,6 +36,24 @@ class Post(object):
         self.Id = attrib_dict['Id']
         self.uri = create_uri(self.Id)
 
+    def _parse(self, attrib_dict):
+        '''
+        Parses the attribute dictionary into a new
+        dictionary that contains only relevant attributes
+        where the keys are the predicates, e.g. 'dc:title',
+        and the values are the objects, e.g. 'Moby Dick".
+        '''
+        # For question posts, answers are populated after
+        #   all answer posts have been instatiated.
+        # This attribute is simply ignored for answer posts.
+        attribs = {u"sioc:has_reply": []}
+        for key,term in self.attrib_to_preds.items():
+            if key in attrib_dict.keys():
+                pred = term.encode('utf8')
+                obj = format_str(attrib_dict[key]).encode('utf8')
+                attribs[pred] = obj
+        return attribs
+
     def get_attribs_string(self):
         '''
         Creates a list of SUBJECT-PREDICATE-OBJECT triples
@@ -46,84 +64,90 @@ class Post(object):
             for attrib in question.attribs_strs:
                 out.write(attrib)
         '''
-
         attribs_strs = []
-
         for pred,obj in self.attribs.items():
+
             # E.g. 'dc:title' or 'sioc:content'
             if type(obj) == str:
-                string = '{0} {1} {2} .\n' .format(self.uri, pred, obj)
-                attribs_strs.append(string)
+                # I always want title first for questions.
+                if pred == u'dc:title':
+                    string = '{0} {1} {2} ;\n' .format('\t', pred, obj)
+                    attribs_strs.insert(0, string)
+                else:
+                    string = '{0} {1} {2} ;\n' .format('\t', pred, obj)
+                    attribs_strs.append(string)
+
             # E.g. 'tsioc:Answer'
             elif type(obj) == list:
-                for elem in obj:
-                    string = '{0} {1} {2} .\n' .format(self.uri, pred, elem.uri)
+                for i in range(len(obj)):
+                    # Only one reply.
+                    if len(obj) == 1:
+                        string = '{0} {1} {2} ;\n' .format('\t', pred, obj[i].uri)
+                    # First element.
+                    elif i == 0:
+                        string = '{0} {1} {2},\n' .format('\t', pred, obj[i].uri)
+                    # Last element.
+                    elif i+1 == len(obj):
+                        string = '{0} {1} ;\n' .format('\t\t', obj[i].uri)
+                    else:
+                        string = '{0} {1},\n' .format('\t\t', obj[i].uri)
                     attribs_strs.append(string)
+
             # E.g. 'tsioc:BestAnswer'
             elif type(obj) == Answer or type(obj) == Question:
-                string = '{0} {1} {2} .\n' .format(self.uri, pred, obj.uri)
-                attribs_strs.append(string)
-                
+                # I always want has_parent first for answers.
+                if pred == u'sioc:has_parent':
+                    string = '{0} {1} {2} ;\n' .format('\t', pred, obj.uri)
+                    attribs_strs.insert(0, string)
+                else:
+                    string = '{0} {1} {2} ;\n' .format('\t', pred, obj.uri)
+                    attribs_strs.append(string)
+
+        # Make sure last item printed for this URI ends with a full-stop.
+        attribs_strs[-1] = attribs_strs[-1][:-2] + '.'
+
+        first_line = '{0} {1} {2} ;\n' .format(self.uri, "a", self.type_str)
+        attribs_strs.insert(0, first_line)
+        if self.type_str == u"tsioc:Answer":
+            if self.best_answer:
+                second_line = '{0} {1} {2} ;\n' .format('\t', "a", u"tsioc:BestAnswer")
+                attribs_strs.insert(1, second_line)
+
         return attribs_strs
 
 
 class Answer(Post):
 
+    # Specify which attributes we care about for answer posts.
+    attrib_to_preds = {
+        u"Body": u"sioc:content",
+        u"ParentId" : u"sioc:has_parent",
+        u"CreationDate": u"dc:date"
+    }
+
     def __init__(self, attrib_dict):
         Post.__init__(self, attrib_dict)
         self.parent_Id = attrib_dict['ParentId']
         self.attribs = self._parse(attrib_dict)
-
-    def _parse(self, attrib_dict):
-
-        # Specify which attributes we care about for quesiton posts.
-        attrib_to_preds = {
-            u"Body": u"sioc:content",
-            u"ParentId" : u"sioc:has_parent"
-        }
-
-        # The answer list is populated after we create uris
-        # for each answer post.
-        attribs = {u"tsioc:Answer": []}
-        
-        for key,term in attrib_to_preds.items():
-            if key in attrib_dict.keys():
-                pred = term.encode('utf8')
-                obj = format_str(attrib_dict[key]).encode('utf8')
-                attribs[pred] = obj
-
-        return attribs
+        self.type_str = u"tsioc:Answer"
+        # Is assigned true if this answer is also the best answer.
+        self.best_answer = False
 
 
 class Question(Post):
 
+    # Specify which attributes we care about for question posts.
+    attrib_to_preds = {
+        u"Title": u"dc:title",
+        u"Body": u"sioc:content",
+        u"CreationDate": u"dc:date"
+    }
+
     def __init__(self, attrib_dict):
         Post.__init__(self, attrib_dict)
         self.attribs = self._parse(attrib_dict)
-
-    def _parse(self, attrib_dict):
-        '''
-        Parses the attribute dictionary into a new
-        dictionary that contains only relevant attributes
-        where the keys are the predicates, e.g. 'dc:title',
-        and the values are the objects, e.g. 'Moby Dick".
-        '''
-
-        # Specify which attributes we care about for quesiton posts.
-        attrib_to_preds = {
-            u"Title": u"dc:title",
-            u"Body": u"sioc:content",
-            u"AcceptedAnswerId" : u"tsioc:BestAnswer"
-        }
-
-        # The answer list is populated after we create uris
-        # for each answer post.
-        attribs = {u"tsioc:Answer": []}
-        
-        for key,term in attrib_to_preds.items():
-            if key in attrib_dict.keys():
-                pred = term.encode('utf8')
-                obj = format_str(attrib_dict[key]).encode('utf8')
-                attribs[pred] = obj
-
-        return attribs
+        self.type_str = u"tsioc:Question"
+        try:
+            self.best_answer_Id = attrib_dict[u"AcceptedAnswerId"]
+        except:
+            self.best_answer_Id = None
